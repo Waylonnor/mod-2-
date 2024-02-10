83715d9ea25b98b4121cdd4d30f9611087b72197
Waylonnor <157814887+Waylonnor@users.noreply.github.com>
2024-02-10 18:57:21 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
@@ -21,21 +19,12 @@ contract Assessment {
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -47,14 +36,31 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function checkLoanEligibility(uint256 _creditScore) public pure returns(uint256) {
+        if (_creditScore > 720) {
+            return 10000;
+        } else if (_creditScore > 700) {
+            return 7000;
+        } else if (_creditScore > 600) {
+            return 6000;
+        } else if (_creditScore > 400) {
+            return 5000;
+        } else {
+            return 0;
+        }
+    }
+    
+    function viewLoanBrochure() public pure returns (string memory) {
+        string memory brochure = "Loan Details:\n";
+        brochure = string(abi.encodePacked(brochure, "If your credit score is above 720, you are eligible for a loan of $10,000.\n"));
+        brochure = string(abi.encodePacked(brochure, "If your credit score is above 700, you are eligible for a loan of $7,000.\n"));
+        brochure = string(abi.encodePacked(brochure, "If your credit score is above 600, you are eligible for a loan of $6,000.\n"));
+        brochure = string(abi.encodePacked(brochure, "If your credit score is above 500, you are eligible for a loan of $5,000.\n"));
+        return brochure;
     }
 }
